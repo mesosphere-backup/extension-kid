c7286d7e3bbfc4e3082c512bfd58a7c60e85a5f7
Dmitriy Rozhkov <drozhkov@mesosphere.io>
2018-12-21 19:36:56 +0100

    feat: initial release

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  roots: ["<rootDir>/src"],
+  setupFiles: ["./jest/setupEnv.js"],
+  transform: {
+    "^.+\\.tsx?$": "ts-jest"
+  },
+  testURL: "http://localhost",
+  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"]
+};
